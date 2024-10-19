fb792070393ad1946c72bc5d9eb2f14926bd6571
Andrew Limond <andrew.limond@epfl.ch>
2024-10-19 22:09:29 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+    return (
+        <div className="h-screen w-screen flex justify-center align-middle items-center">
+            <div className="text-3xl font-bold text-gray-300">
+                Bienvenue dans la Ligue des Légendes 🔥
+            </div>
+        </div>
+    );
+}
