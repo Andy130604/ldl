29801a254a99c0b12f2dd9ef27ffd80aa94f1ed7
Andrew Limond <andrew.limond@epfl.ch>
2024-10-20 22:06:47 +0200

    Setup supabase and basic fetch function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import "./App.css";
+import { supabase } from "./createClient";
+import { useState, useEffect } from "react";
 
 export default function App() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        fetchUsers();
+    }, []);
+
+    async function fetchUsers() {
+        const { data } = await supabase.from("users").select("*");
+        setUsers(data);
+        console.log(data);
+    }
+
     return (
         <div className="h-screen w-screen flex justify-center align-middle items-center">
             <div className="text-3xl font-bold text-gray-300">
