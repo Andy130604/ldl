ed0c2aa5984c5117b27908dc88c79f146a258df9
Andrew Limond <andrew.limond@epfl.ch>
2024-10-23 23:15:13 +0200

    Start working on updating rankings

--- src/services/database.js
+++ src/services/database.js
@@ -1,4 +1,5 @@
 import { supabase } from "./supabaseClient";
+import { getWinnerIdByScore, isValidScore } from "./utils";
 
 export const fetchAllPlayers = async () => {
     const { data, error } = await supabase
@@ -29,6 +30,24 @@ export const createMatch = async (player1_id, player2_id, scheduled_at) => {
     return data;
 };
 
-export const writeMatchScore = async (match, score) => {
-    
+export const writeMatchScore = async (match, score, players) => {
+    if (!isValidScore(score)) {
+        console.error("Invalid score format");
+        return;
+    }
+    const winner_id = getWinnerIdByScore(score, match.player1_id, match.player2_id);
+
+    // Update the match with the score and winner
+    const { error } = await supabase
+        .from("matches")
+        .update({ score: score, winner_id: winner_id, played: true })
+        .eq({ id: match.id });
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    // Update the players with the new ranks or matches streaks
+
 };
